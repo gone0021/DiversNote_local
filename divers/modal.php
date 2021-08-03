6d36577263dc73837fd611a232990249d03f9e01
gen.n <gt.4.pff.gi@gmail.com>
2021-08-03 19:22:48 +0900

    編集：モーダルのスタイルを修正

--- divers/modal.php
+++ divers/modal.php
@@ -9,12 +9,6 @@
          <!-- タイトル部分 -->
          <div id="mdlTop">
             <div class="mdlTopGrid">
-               <div class="mdlTitle">
-                  <label>タイトル：
-                     <input type="text" name="title" id="" class="mTitle" v-model="mTitle" :disabled="dis" placeholder="未入力" required>
-                  </label>
-               </div>
-
                <div class="mdlBtn">
                   <template v-if="btnNew">
                      <input type="submit" :name="mSub" class="btn" value="登録" />
@@ -33,13 +27,21 @@
                      <button type="button" class="btn" @click="onDel">削除</button>
                   </template>
                </div>
+
+               <div class="mdlTitle">
+                  <label>タイトル：
+                     <input type="text" name="title" id="" class="mTitle" v-model="mTitle" :disabled="dis" placeholder="未入力" required>
+                  </label>
+               </div>
+
+
             </div>
          </div>
          <!-- 幅の調整 -->
          <div id="adjTop"></div>
 
          <!-- アイテム詳細 -->
-         <div class="m2 mdlGroup">
+         <div class="mdlGroup">
             <div id="number" class="my-1">
                <label>No.
                   <input type="text" name="dive_num" id="" class="" v-model="mDiveNum" :disabled="dis" :style="border" :placeholder="next_num" required>
@@ -53,7 +55,7 @@
             </div>
          </div>
 
-         <div class="m2 mdlGroup">
+         <div class="mdlGroup">
             <div id="number" class="my-1">
                <label>地域：
                   <input type="text" name="erea_name" id="" class="" v-model="mErea" :disabled="dis" :style="border" placeholder="未入力" required>
@@ -69,7 +71,7 @@
          <!-- 最初から表示 -->
          <div class="acdTitle">基本情報</div>
          <div class="acdItem">
-            <div class="m2 mdlGroup">
+            <div class="mdlGroup">
 
                <div id="shop" class="">
                   <label>ショップ名：
@@ -88,7 +90,7 @@
                </div>
             </div>
 
-            <div class="m2 mdlGroup">
+            <div class="mdlGroup">
                <div id="startTime" class=" my-1">
                   <label>開始時間：
                      <input type="time" name="start_time" id="" class="" v-model="mStartTime" :disabled="dis" :style="border">
@@ -100,7 +102,7 @@
                   </label>
                </div>
             </div>
-            <div class="m2 mdlGroup">
+            <div class="mdlGroup">
                <div id="startTime" class=" my-1">
                   <label>最大水深 (m)：
                      <input type="text" name="max_depth" id="" class="" v-model="mMaxDepth" :disabled="dis" :style="border" placeholder="未入力">
@@ -116,7 +118,7 @@
 
          <div class="acdTitle">タンク類</div>
          <div class="acdItem">
-            <div class="m2 mdlGroup">
+            <div class="mdlGroup">
                <div id="tankMaterial" class=" my-1">
                   <label>タンク素材：
                      <select name="tank_material" id="" class="" v-model="mTankMaterial" :disabled="dis" :style="border">
@@ -135,7 +137,7 @@
                </div>
             </div>
 
-            <div class="m1 mdlGroup">
+            <div class="mdlGroup">
                <div id="enrche" class=" my-1">
                   <label>エンリッチ：
                      <select name="is_enriche" id="" class="" v-model="mIsEnriche" :disabled="dis" :style="border">
@@ -148,7 +150,7 @@
                </div>
             </div>
 
-            <div class="m2 mdlGroup">
+            <div class="mdlGroup">
                <div id="startAir" class=" my-1">
                   <label>開始残圧 (bar)：
                      <input type="text" name="start_air" id="" class="" v-model="mStartAir" :disabled="dis" :style="border" placeholder="未入力">
@@ -165,7 +167,7 @@
 
          <div class="acdTitle">気候・海峡</div>
          <div class="acdItem">
-            <div class="m3 mdlGroup">
+            <div class="mdlGroup">
 
                <div id="temp" class=" my-1">
                   <label>気温： (℃)：
@@ -186,7 +188,7 @@
                </div>
             </div>
 
-            <div class="m3 mdlGroup">
+            <div class="mdlGroup">
                <div id="weather" class=" my-1">
                   <label>天候：
                      <select name="weather" id="" class="" v-model="mWeather" :disabled="dis" :style="border">
@@ -226,7 +228,7 @@
 
          <div class="acdTitle">その他器材</div>
          <div class="acdItem">
-            <div class="m2 mdlGroup">
+            <div class="mdlGroup">
                <div id="suitType" class=" my-1">
                   <label>スーツ：
                      <select name="suit_type" id="" class="" v-model="mSuitType" :disabled="dis" :style="border">
@@ -249,27 +251,32 @@
 
          <div class="acdTitle">コメント</div>
          <div class="acdItem">
-            <div id="commnet" class="m1 mdlGroup">
+            <div id="commnet" class="mdlGroup">
                <textarea name="comment" id="" cols="1" rows="5" v-model="mComment" :disabled="dis"></textarea>
             </div>
          </div>
 
          <div class="acdTitle">地図</div>
          <div class="acdItem">
-            <div id="map" class="m1 mdlGroup">
-               <template v-if="isEdit || isNew">
+            <template v-if="isEdit || isNew">
+               <div id="map" class="mdlGroup">
                   <input type="text" name="map_link" id="" class="mMap" v-model="mMap" :disabled="dis" :style="border" placeholder="未入力">
+               </div>
+               <div id="map" class="mdlGroup">
                   <p><a href="https://www.google.co.jp/maps/" target="blank">GoogleMap</a>から「共有→地図を埋め込む」のURLを貼り付けてください</p>
-               </template>
-               <template v-if="isDetail">
-                  <div v-html="mMap"></div>
-               </template>
-            </div>
+               </div>
+            </template>
+            <template v-if="isDetail">
+               <div id="map" class="mdlGroup">
+                  <div v-if="mMap" v-html="mMap"></div>
+                  <div v-else>地図登録なし</div>
+               </div>
+            </template>
          </div>
 
          <div class="acdTitle">仲間</div>
          <div class="acdItem">
-            <div class="m2 mdlGroup">
+            <div class="mdlGroup">
                <div id="buddyName" class="my-1">
                   <label>バディ名：
                      <input type="text" name="buddy_name" id="" class="" v-model="mBuddy" :disabled="dis" :style="border" placeholder="未入力">
@@ -282,7 +289,7 @@
                   </label>
                </div>
             </div>
-            <div class="m1 mdlGroup">
+            <div class="mdlGroup">
                <div id="instructorNum" class="my-1">
                   <label>イントラ番号：
                      <input type="text" name="buddy_name" id="" class="" v-model="mInstructorNum" :disabled="dis" :style="border" placeholder="未入力">
@@ -295,7 +302,7 @@
 
          <!-- <div id="signeTitle" :class="{ 'acdTitle': true, 'text-danger': signeTitleOpen }" @click="onSigne">サイン</div>
          <div id="signeBox" class="acdItem">
-            <div id="signe" class="m1 mdlGroup">
+            <div id="signe" class="mdlGroup">
                <template v-if="isEdit || isNew">
                   <div class="draw-canvas" id="canvas" width="640" height="150">
                      <canvas width="640" height="150" ref="canvas" v-on:mousedown="drawStart" v-on:mouseup="drawEnd" v-on:mouseout="drawEnd" v-on:mousemove="draw" v-on:touchstart="drawTouchStart" v-on:touchend="drawEnd" v-on:touchcancel="drawEnd" v-on:touchmove="drawTouch" v-on:gesturestart="drawTouchStart" v-on:gesturechange="drawEnd" v-on:gestureend="drawTouch"></canvas>
