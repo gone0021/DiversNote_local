c59aea3d4b6328609e8adbb3eb46b313c78c00f6
gen.n <gt.4.pff.gi@gmail.com>
2021-10-05 17:50:19 +0900

    編集：アコーディオンの記述箇所を変更

--- js/divers.js
+++ js/divers.js
@@ -1,9 +1,4 @@
 $(function () {
-   // 読み込み時
-   $(window).on('load', function (e) {
-      //
-   });
-
    $(".onClose").click(function (e) {
       $("#menubar-s").hide();
       // e.preventDefault();
@@ -12,6 +7,51 @@ $(function () {
    // 画面幅が変わった時
    $(window).resize(function () {
       //
+   });
+
+   // モーダル用アコーディオン
+   var flg = 0;
+   $(".acdTitle").on("click", function () {
+      // if (flg == 0) {
+      if (!$(this).hasClass('acdOpen')) {
+         $(this).next().slideDown(200);
+         $(this).addClass("acdOpen");
+         flg = 1;
+      } else {
+         $(this).next().slideUp(200);
+         $(this).removeClass("acdOpen");
+         flg = 0;
+      }
+   });
+   $(".acdTitleOpen").on("click", function () {
+      // if (flg == 0) {
+      if (!$(this).hasClass('acdOpen')) {
+         $(this).next().slideDown(200);
+         $(this).addClass("acdOpen");
+         flg = 1;
+      } else {
+         $(this).next().slideUp(200);
+         $(this).removeClass("acdOpen");
+         flg = 0;
+      }
+   });
+
+
+   // パスワード再設定ページのアコーディオン
+   $(".editPass").on("click", function () {
+      console.log("click");
+
+      if (!$(this).hasClass('acdOpen')) {
+         $(this).next().slideDown(200);
+         $(this).addClass("acdOpen");
+         $(".new_pass").prop("disabled", false);
+         $(".new_pass").prop("required", true);
+      } else {
+         $(this).next().slideUp(200);
+         $(this).removeClass("acdOpen");
+         $(".new_pass").prop("disabled", true);
+         $(".new_pass").prop("required", false);
+      }
    });
 
    /**
