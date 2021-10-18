208e20377b80045a912176c5e82e54151006cbc4
gen.n <gt.4.pff.gi@gmail.com>
2021-10-18 20:47:53 +0900

    追加：footerのつ追加

--- footer.php
+++ footer.php
@@ -1,24 +1,19 @@
 <footer>
    <div id="footermenu" class="inner">
       <ul>
-         <li class="title">タイトル</li>
-         <li><a href="#">メニューサンプル</a></li>
-         <li><a href="#">メニューサンプル</a></li>
+         <li class="title"><a href="<?= $url ?>/">HOME</a></li>
       </ul>
       <ul>
-         <li class="title">タイトル</li>
-         <li><a href="#">メニューサンプル</a></li>
-         <li><a href="#">メニューサンプル</a></li>
+         <li class="title">ENTRY</li>
+         <li><a href="<?= $url ?>/auth/login/">ログイン</a></li>
+         <li><a href="<?= $url ?>/auth/register/">新規登録</a></li>
+         <li><a href="<?= $url ?>/auth/password/">お問い合わせ</a></li>
+         
       </ul>
       <ul>
-         <li class="title">タイトル</li>
-         <li><a href="#">メニューサンプル</a></li>
-         <li><a href="#">メニューサンプル</a></li>
-      </ul>
-      <ul>
-         <li class="title">タイトル</li>
-         <li><a href="#">メニューサンプル</a></li>
-         <li><a href="#">メニューサンプル</a></li>
+         <li class="title">OTHER</li>
+         <li><a href="<?= $url ?>/contact.php">お問い合わせ</a></li>
+         <li><a href="<?= $url ?>/weight.php">適正ウエイト</a></li>
       </ul>
    </div>
    <!--/#footermenu-->
@@ -32,7 +27,7 @@
       </ul>
 
       <!-- <small>Copyright&copy; <a href="index.html">SAMPLE COMPANY</a> All Rights shimaIchi.</small> -->
-      <small>Copyright&copy; All Rights shimaIchi.</small>
+      <small>Copyright&copy; All Rights Divers Note.</small>
    </div>
    <!--/#copyright-->
 
