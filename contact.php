2d7362714fb0f273f057d60e74f72c969df0f8b3
gen.n <gt.4.pff.gi@gmail.com>
2021-10-18 20:30:15 +0900

    編集：formの追加

--- contact.php
+++ contact.php
@@ -9,7 +9,6 @@ $rootUrl = $_SERVER['SERVER_NAME'];
 $rootUrl .= "/data/DiversNote_local";
 $url = 'http://' . $rootUrl;
 
-// --- contact用 ---
 // セッションスタート
 SessionUtil::sessionStart();
 
@@ -36,25 +35,29 @@ $_SESSION['token'] = $token;
                <section id="contact">
                   <h2>CONTACT<span>お問い合わせ</span></h2>
                   <h3>お問い合わせ内容をご入力ください</h3>
-                  <table class="ta1">
-                     <tr>
-                        <th>お名前</th>
-                        <td><input type="text" name="name" size="30" class="ws" disabled></td>
-                     </tr>
-                     <tr>
-                        <th>メールアドレス</th>
-                        <td><input type="text" name="email" size="30" class="ws" disabled></td>
-                     </tr>
-                     <tr>
-                        <th>お問い合わせ内容</th>
-                        <td><textarea name="message" cols="30" rows="10" class="wl" disabled></textarea></td>
-                     </tr>
-                  </table>
+                  <form action="">
+                  <input type="hidden" name="token" id="" value="<?= $token ?>">
 
-                  <p class="c">
-                     <input type="submit" value="内容を確認する" class="btn" disabled>
-                  </p>
+                     <table class="ta1">
+                        <tr>
+                           <th>お名前</th>
+                           <td><input type="text" name="name" size="30" class="ws" disabled></td>
+                        </tr>
+                        <tr>
+                           <th>メールアドレス</th>
+                           <td><input type="text" name="email" size="30" class="ws" disabled></td>
+                        </tr>
+                        <tr>
+                           <th>お問い合わせ内容</th>
+                           <td><textarea name="message" cols="30" rows="10" class="wl" disabled></textarea></td>
+                        </tr>
+                     </table>
 
+                     <p class="c">
+                        <input type="submit" value="内容を確認する" class="btn" disabled>
+                     </p>
+
+                  </form>
                   <p class="c">※ 現在ご利用頂けません</p>
 
                </section>
