490fb0bd1d69e54c7b49bd01facbfb572a5460b4
gen.n <gt.4.pff.gi@gmail.com>
2022-01-05 01:57:19 +0900

    編集：一括チェックの追加、未入力に対するアラートの追加

--- js/list.js
+++ js/list.js
@@ -18,19 +18,23 @@
          addChecked: [],
          addTags: [],
          addLists: [],
+         addNum: [], // 追加分の数を数える値
+
+         // checkboxのbind
+         isChecked: false,
       },
       created: function () {
-         console.log('--- created app.js ---');
+         // console.log('--- created app.js ---');
          // console.log(this.url);
          this.getList();
       },
       mouted: function () {
-         console.log('--- mounted app.js ---');
+         // console.log('--- mounted app.js ---');
          // console.log(token);
          // console.log(root);
       },
       updated: function () {
-         console.log('--- updated app.js ---');
+         // console.log('--- updated app.js ---');
       },
       methods: {
          /**
@@ -58,9 +62,32 @@
          },
 
          /**
+          * 登録済のリストのチェックを一括で反転させる
+          * @param {*} e 
+          */
+         onIsChecked: function (e) {
+            // v_checkedの値を編集
+            // 記録用にkey,valueの書き方で書いてるだけで意味はない
+            for (var [key, val] of this.lists.entries()) {
+               if (!this.isChecked) {
+                  val.v_checked = true;
+               } else {
+                  val.v_checked = false;
+               }
+            }
+
+            // isCheckedの値を編集
+            if (!this.isChecked) {
+               this.isChecked = true
+            } else {
+               this.isChecked = false
+            }
+         },
+
+         /**
           * submit
           */
-         submitList: function () {
+         submitList: function (e) {
             var valid;
             // for (var val, i of this.lists) {
             //    valid = this.validVal(i);
@@ -69,47 +96,99 @@
             // if (valid) {
             const params = new URLSearchParams();
 
+            // tokenとuser_idを保存
             params.append("token", this.token);
             params.append("user_id", this.user_id);
+
+            // 既存の値（更新分）
             for (var i = 0; i < this.lists.length; i++) {
+               // 空のtag_nameを編集
+               if (this.lists[i].tag_name === "") {
+                  // alert("non");
+                  this.lists[i].tag_name = "未記入";
+               } else {
+                  this.lists[i].tag_name = this.lists[i].tag_name;
+               }
+
+               // is_checkedの値をdbに保存する値に編集
                if (this.lists[i].v_checked === false) {
                   this.lists[i].is_checked = 0;
                } else {
                   this.lists[i].is_checked = 1;
                }
+
                // dbに不要なキーを削除
                delete this.lists[i].v_checked;
 
-               this.setParam(params, i);
+               // 更新分の値をセット
+               // メソッドを作成しているが一度しか使用していないため一旦は使わない
+               params.append("tag_name[]", this.lists[i].tag_name);
+               params.append("list_name[]", this.lists[i].list_name);
+               params.append("is_checked[]", this.lists[i].is_checked);
+               // this.setParam(params, i);
             }
 
             // 追加分
+            // 追加されたリストの数
+            var count = this.addLists.length;
+
+            // 代入に使用する変数：dbに保存する値に編集するのに使用
             var tagName = [];
+            var listName = [];
             var checked = [];
-            if (this.addLists.length) {
-               // console.log("is new todo");
-               for (var i = 0; i < this.addLists.length; i++) {
-                  // tag_nameの値
+
+            // エラーメッセージ
+            var msg = ["リストが入力されていません。\n新しいタグは追加されませんがよろしいですか？"].join("\n");
+
+            // タイトルのみ入力された場合はアラート：addListsに追加があった場合：追加がないとcountは0のままのためわざわざ作成する
+            if (this.addTags[0] !== undefined && this.addLists[0] === undefined) {
+               if (!window.confirm(msg)) {
+                  e.preventDefault()
+               }
+            }
+
+            // 追加があった場合
+            if (count) {
+               // console.log("is new list");
+               for (var i = 0; i < count; i++) {
+                  // 空のtag_nameを編集
                   if (this.addTags[i] === undefined) {
                      // alert("non");
-                     tagName[i] = "なし";
+                     tagName[i] = "未記入";
                   } else {
                      tagName[i] = this.addTags[i];
                   }
 
-                  // is_checkedの値
+                  // 空のlist_nameを編集
+                  if (this.addLists[i] === undefined) {
+                     // alert("non");
+                     listName[i] = "未記入";
+                  } else {
+                     listName[i] = this.addLists[i];
+                  }
+
+                  // is_checkedの値をdbに保存する値に編集
                   if (this.addChecked[i] === true) {
                      checked[i] = 1;
                   } else {
                      checked[i] = 0;
                   }
 
+                  // タイトルのみ入力された場合はアラート：addListsに追加があった場合
+                  if (this.addTags[i + 1] !== undefined && this.addLists[i + 1] === undefined) {
+                     // alert("bbb");
+                     if (!window.confirm(msg)) {
+                        e.preventDefault()
+                     }
+                  }
+
+                  // 追加分の値をセット
                   params.append("tag_name[]", tagName[i]);
-                  params.append("list_name[]", this.addLists[i]);
+                  params.append("list_name[]", listName[i]);
                   params.append("is_checked[]", checked[i]);
                }
             } else {
-               // 何もしない：デバッグ用の出力
+               // 何もしない：デバッグ用の出力を記述する
                // console.log("non new todo");
             }
 
@@ -117,7 +196,7 @@
             this.postAxios(url, params);
             this.resetVal();
             alert("更新されました");
-            // }
+            // location.reload();
          },
 
 
@@ -134,7 +213,7 @@
                   this.getList();
                }.bind(this)
             ).catch(function (e) {
-               console.error(e);
+               // console.error(e);
                alert("入力に不備がありました。");
             });
          },
@@ -152,16 +231,28 @@
                   this.getLists = res.data;
                   this.lists = res.data;
 
-                  for (var i = 0; i < this.lists.length; i++) {
+                  for (var val of this.lists) {
                      // is_checkedの変換
-                     if (this.lists[i].is_checked === "0") {
-                        this.lists[i].v_checked = false;
+                     if (val.is_checked === "0") {
+                        val.v_checked = false;
                      } else {
-                        this.lists[i].v_checked = true;
+                        val.v_checked = true;
                      }
                      // tag_nameの変換
-                     if (this.lists[i].tag_name == null) {
-                        this.lists[i].tag_name = "なし";
+                     if (val.tag_name == null) {
+                        val.tag_name = "なし";
+                     }
+                  }
+
+                  // isCheckedの値を編集
+                  // 0があればisCheckedをfalseで保存するためbrakeを入れる
+                  // 全てが1の場合のみisCheckedをtrueにする
+                  for (var val of this.lists) {
+                     if (val.is_checked === "0") {
+                        this.isChecked = false;
+                        break;
+                     } else {
+                        this.isChecked = true;
                      }
                   }
                   // console.log(this.lists);
@@ -217,6 +308,7 @@
             this.addTags = [];
             this.addLists = [];
             this.addChecked = [];
+            this.isChecked = false;
          },
 
          /**
