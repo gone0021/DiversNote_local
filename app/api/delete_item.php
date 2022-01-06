acbeddff783327773fee16ee811d8a5262f7d68a
gen.n <gt.4.pff.gi@gmail.com>
2022-01-07 03:20:23 +0900

    編集：csrf対策の追加

--- app/api/delete_item.php
+++ app/api/delete_item.php
@@ -1,22 +1,22 @@
 <?php
 require_once('../config.php');
 
+use app\util\CommonUtil;
 use app\controllers\ItemController;
 
-// 値を渡す側で処理させるため不使用：サンプルで残している
-// $request_body = file_get_contents('php://input'); 
-// $data = json_decode($request_body,true);
+// // CSRF対策）
+CommonUtil::csrf($_SESSION['token'], $_POST['token']);
 
 // echo '<pre>';
 // var_export($_POST);
 // echo '</pre>';
+
+// 削除時にサインや写真も削除する予定のためcontrollerを経由させる
 try {
    $conItem = new ItemController();
-   $conItem->soft_delete($_POST);
+   $conItem->softDelete($_POST);
 } catch (Exception $e) {
    // var_dump($e);exit;
    header('Location: ./');
 }
-// echo '<br />';
-// var_dump($ret);
-// return json_encode($ret);
+
