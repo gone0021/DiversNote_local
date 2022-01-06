de374842d42f6a8960310bb85f0664ebbcde6b86
gen.n <gt.4.pff.gi@gmail.com>
2022-01-07 04:28:18 +0900

    編集：不要箇所の削除

--- app/api/get_item_photo.php
+++ app/api/get_item_photo.php
@@ -4,10 +4,6 @@ require_once('../config.php');
 use app\model\BaseModel;
 use app\model\PhotoModel;
 
-// $root = $_SERVER['DOCUMENT_ROOT'];
-// $root .= "/data/DiversNote_local";
-// require_once($root . "/app/model/PhotoModel.php");
-
 $item_id = $_GET['item_id'];
 
 try {
