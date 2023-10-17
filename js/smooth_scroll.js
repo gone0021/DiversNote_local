6e93903e5f079beb25c1d912770002cf34a94137
gone0021 <gt.4.pff.gi@gmail.com>
2023-10-18 02:12:55 +0900

    idの変更の伴う修正、不要箇所の削除

--- js/smooth_scroll.js
+++ js/smooth_scroll.js
@@ -2,9 +2,6 @@ $(function () {
    let adj = 0;
    let pageTop = 0;
    let home = 0;
-   let about = 0;
-   let news = 0;
-   let contact = 0;
    let entry = 0;
 
    // DOM読み込み時
@@ -15,7 +12,7 @@ $(function () {
    $(window).resize(function () {
       calcAdj();
       setSize();
-      console.log(entry);
+      // console.log(entry);
    });
 
    let time = 500;
@@ -28,33 +25,15 @@ $(function () {
 
    // home
    $('.onHome').click(function () {
-      // console.log("home");
+      console.log("home");
       console.log(adj);
       console.log(home);
       $("html").animate({ scrollTop: pageTop }, time);
    });
 
-   // about
-   $('.onAbout').click(function () {
-      // console.log("about");
-      $("html").animate({ scrollTop: about }, time);
-   });
-
-   // smaple
-   $('.onNews').click(function () {
-      // console.log("sample");
-      $("html").animate({ scrollTop: news }, time);
-   });
-
-   // contact
-   $('.onContact').click(function () {
-      // console.log("contact");
-      $("html").animate({ scrollTop: contact }, time);
-   });
-
    // entry
    $('.onEntry').click(function () {
-      // console.log("entry");
+      console.log("entry");
       $("html").animate({ scrollTop: entry }, time);
    });
 
@@ -78,9 +57,7 @@ $(function () {
     */
    function setSize() {
       pageTop = $('html').offset().top;
-      home = $('#new').offset().top - adj;
-      news = $('#new').offset().top - adj;
-      about = $('#about').offset().top - adj;
+      // home = $('#news').offset().top - adj;
       entry = $('#entry').offset().top - adj;
    }
 });
